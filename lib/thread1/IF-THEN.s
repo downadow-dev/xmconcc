7923e90f15e35b117e4aa205b8c255979c779690
Sviatoslav <downadow@ro.ru>
2024-08-12 15:47:11 +0300

    upd.

--- lib/thread1/IF-THEN.s
+++ lib/thread1/IF-THEN.s
@@ -1,4 +1,49 @@
 
+__1_?:
+	dec %R_FA_19%
+	ild %R_FA_19%, %R_FA_11%
+	dec %R_FA_19%
+	ild %R_FA_19%, %R_FA_10%
+	
+	mov2 %R_FA_12%, <LIB_1_?_continue>
+	mov %R_FA_13%, 1
+	if %R_FA_10% == %R_FA_13% && %R_FA_11% == %R_FA_13%, %R_FA_12%
+	
+	mov %R_FA_10%, 0
+	isv %R_FA_10%, %R_FA_19%
+	inc %R_FA_19%
+	jmp %R_FA_18%
+	
+	LIB_1_?_continue:
+	
+	mov %R_FA_10%, 1
+	isv %R_FA_10%, %R_FA_19%
+	inc %R_FA_19%
+	jmp %R_FA_18%
+
+__1_|?:
+	dec %R_FA_19%
+	ild %R_FA_19%, %R_FA_11%
+	dec %R_FA_19%
+	ild %R_FA_19%, %R_FA_10%
+	
+	mov2 %R_FA_12%, <LIB_1_|?_continue>
+	mov %R_FA_13%, 1
+	if %R_FA_10% == %R_FA_13%, %R_FA_12%
+	if %R_FA_11% == %R_FA_13%, %R_FA_12%
+	
+	mov %R_FA_10%, 0
+	isv %R_FA_10%, %R_FA_19%
+	inc %R_FA_19%
+	jmp %R_FA_18%
+	
+	LIB_1_|?_continue:
+	
+	mov %R_FA_10%, 1
+	isv %R_FA_10%, %R_FA_19%
+	inc %R_FA_19%
+	jmp %R_FA_18%
+
 __1_=?:
 	dec %R_FA_19%
 	ild %R_FA_19%, %R_FA_11%
