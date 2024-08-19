dc047ad2c6fe8f4d134ac357ac5f2747cc8a890d
Sviatoslav <downadow@ro.ru>
2024-08-19 23:46:07 +0300

    better security

--- lib/main/BASIC_MEMORY_OPERATIONS.s
+++ lib/main/BASIC_MEMORY_OPERATIONS.s
@@ -5,6 +5,16 @@ __0_=:
 	dec %R_FA_9%
 	ild %R_FA_9%, %R_FA_1%
 	
+	mov %R_FA_2%, 0
+	mov2 %R_FA_3%, <LIB_=_continue>
+	if %R_USER% == %R_FA_2%, %R_FA_3%
+	
+	mov %R_RETURN_CODE%, 30
+	mov2 %R_FA_2%, 6900000
+	if %R_FA_0% < %R_FA_2%, UR17
+	
+	LIB_=_continue:
+	
 	isv %R_FA_1%, %R_FA_0%
 	
 	jmp %R_FA_8%
--- lib/thread1/BASIC_MEMORY_OPERATIONS.s
+++ lib/thread1/BASIC_MEMORY_OPERATIONS.s
@@ -5,6 +5,16 @@ __1_=:
 	dec %R_FA_19%
 	ild %R_FA_19%, %R_FA_11%
 	
+	mov %R_FA_12%, 0
+	mov2 %R_FA_13%, <LIB_1_=_continue>
+	if %R_USER% == %R_FA_12%, %R_FA_13%
+	
+	mov %R_RETURN_CODE%, 30
+	mov2 %R_FA_12%, 6900000
+	if %R_FA_10% < %R_FA_12%, UR17
+	
+	LIB_1_=_continue:
+	
 	isv %R_FA_11%, %R_FA_10%
 	
 	jmp %R_FA_18%
